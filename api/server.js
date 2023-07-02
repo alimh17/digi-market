f9a4fde1bf81b17cb4d92a109610c5ee5f6a2e20
alimh17 <amohamadi17@gmail.com>
2023-07-02 17:57:06 +0330

    cors access

--- api/server.js
+++ api/server.js
@@ -1,55 +1,63 @@
-const path = require('path');
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const path = require("path");
 
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 //! -------------- add middlewares -------------
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //! -------------- enable files upload ------------
-app.use(fileUpload({
-    createParentPath: true
-}))
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
 //! ------------------- set Static files -----------------
 
-app.use("/public", express.static(path.join(__dirname, 'public')))
-
+// eslint-disable-next-line no-undef
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 //! -------------- Routes -------------------------
-app.use('/api/v1', require('./routes/banner'))
-app.use('/api/v1', require('./routes/mobiles'))
-app.use('/api/v1', require('./routes/laptops'))
-app.use('/api/v1', require('./routes/tablets'))
-app.use('/api/v1', require('./routes/consoles'))
-app.use('/api/v1', require('./routes/smartWatch'))
-app.use('/api/v1', require('./routes/offers'))
-
+app.use("/api/v1", require("./routes/banner"));
+app.use("/api/v1", require("./routes/mobiles"));
+app.use("/api/v1", require("./routes/laptops"));
+app.use("/api/v1", require("./routes/tablets"));
+app.use("/api/v1", require("./routes/consoles"));
+app.use("/api/v1", require("./routes/smartWatch"));
+app.use("/api/v1", require("./routes/offers"));
 
 //! -------------- create server -------------------
-const PORT = process.env.PORT || process.env.PORT_API
-const server = http.createServer(app)
+const PORT = process.env.PORT || process.env.PORT_API;
+const server = http.createServer(app);
 
 //! -------------- initial database -----------------
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    authSource: "admin"
-}).then(() => {
+    authSource: "admin",
+  })
+  .then(() => {
     server.listen(PORT, () => {
-        console.log(`server is running on ${PORT}`)
-        console.log(`database connected ${process.env.MONGO_URL}`)
-    })
-}).catch(err => {
-    console.log('database connection failed')
-})
-
-
+      console.log(`server is running on ${PORT}`);
+      console.log(`database connected ${process.env.MONGO_URL}`);
+    });
+  })
+  .catch((err) => {
+    console.log("database connection failed");
+  });
